a4e37af4c0124e9d43eee5426d5dd34ce61c1353
Daniel Molkentin <molkentin@kde.org>
2001-09-01 08:34:04 +0000

    compile(Qt3)
    svn path=/trunk/kdegames/kreversi/; revision=112805

--- app.cpp
+++ app.cpp
@@ -945,10 +945,12 @@ void App::showHighscore(int focusitem) {
     }
     
   QPushButton *b = new QPushButton(i18n("&Close"), dlg);
+#if QT_VERSION < 300
   if(style().guiStyle() == MotifStyle)
     b->setFixedSize(b->sizeHint().width() + 10,
 		    b->sizeHint().height() + 10);
   else
+#endif
     b->setFixedSize(b->sizeHint());
 
   // connect the "Close"-button to done
@@ -987,10 +989,12 @@ QString App::getPlayerName() {
 
   QPushButton *b = new QPushButton(i18n("OK"), dlg);
   b->setDefault(TRUE);
+#if QT_VERSION < 300
   if(style().guiStyle() == MotifStyle)
     b->setFixedSize(b->sizeHint().width() + 10,
 		    b->sizeHint().height() +10);
   else
+#endif
     b->setFixedSize(b->sizeHint());
   connect(b, SIGNAL(released()), dlg, SLOT(accept()));
   connect(e, SIGNAL(returnPressed()), 
