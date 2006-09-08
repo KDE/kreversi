ede7233a39ef17ff9a4c382a5afdaa70c2c06066
Dmitry Suzdalev <dimsuz@gmail.com>
2006-09-08 09:57:16 +0000

    Added license stuff
    svn path=/branches/work/kreversi_rewrite/; revision=582111

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
  *
  * This file is part of the KDE project "KReversi"
  *
- * KREVERSI is free software; you can redistribute it and/or modify
+ * KReversi is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
  *
- * KREVERSI is distributed in the hope that it will be useful,
+ * KReversi is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with KREVERSI; see the file COPYING.  If not, write to
+ * along with KReversi; see the file COPYING.  If not, write to
  * the Free Software Foundation, 51 Franklin Street, Fifth Floor,
  * Boston, MA 02110-1301, USA.
  *
@@ -40,6 +40,7 @@ int main(int argc, char **argv)
             KREVERSI_VERSION, description, KAboutData::License_GPL,
             "(c) 1997-2000, Mario Weilguni\n(c) 2006, Dmitry Suzdalev");
     aboutData.addAuthor("Mario Weilguni",I18N_NOOP("Original author"), "mweilguni@sime.com");
+    aboutData.addAuthor("Inge Wallin",I18N_NOOP("Original author"), "inge@lysator.liu.se");
     aboutData.addAuthor("Dmitry Suzdalev", I18N_NOOP("Game rewrite for KDE4. Current maintainer."), "dimsuz@gmail.com");
     aboutData.addCredit("Mats Luthman", I18N_NOOP("Game engine, ported from his JAVA applet."), 0);
     aboutData.addCredit("Arne Klaassen", I18N_NOOP("Raytraced chips."), 0);
