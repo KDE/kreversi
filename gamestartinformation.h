d1d07bb04754218ecbea6543f72fc0840136f5c6
Denis Kuplyakov <dener.kup@gmail.com>
2013-09-17 19:22:39 +0400

    Documented GameStartInformation class.

--- gamestartinformation.h
+++ gamestartinformation.h
@@ -26,17 +26,38 @@
 
 #include <QString>
 
+/**
+ * Represents information gathered from user by StartGameDialog.
+ */
 class GameStartInformation
 {
 
 public:
+    /**
+     * Indicates player type
+     */
     enum PlayerType {
+        /** Human player */
         Human,
+        /** AI (or Computer) player */
         AI
     };
 
+    /**
+     * PlayerType for each player.
+     * Use it like: type[Black]
+     */
     PlayerType type[2];
+    /**
+     * Name for each player.
+     * Use it like: name[Black]
+     */
     QString name[2];
+    /**
+     * Skill for each player.
+     * Has no sense for Human player
+     * Use it like: skill[Black]
+     */
     int skill[2];
 };
 
